6462a4a1e67f93f137d34640cfd6f1fc8cda4159
Hristo Velikov <hvelikov@comsoft.bg>
2024-10-21 18:48:43 +0300

    test phase 1

--- implementation/src/main/java/bg/borica/mq/camel/routes/TimerRoute.java
+++ implementation/src/main/java/bg/borica/mq/camel/routes/TimerRoute.java
@@ -4,15 +4,22 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.apache.camel.CamelContext;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.DefaultErrorHandlerBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.RouteConfigurationBuilder;
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 import org.apache.camel.component.file.AntPathMatcherGenericFileFilter;
+import org.apache.camel.processor.errorhandler.DefaultErrorHandler;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.sql.SQLException;
 
 /**
  * A simple {@link RouteBuilder}.
  */
 @ApplicationScoped
-public class TimerRoute extends RouteBuilder {  // RouteConfigurationBuilder
+public class TimerRoute extends EndpointRouteBuilder { // RouteBuilder   // EndpointRouteBuilder
     @Inject
     CamelContext context;
 
@@ -21,20 +28,18 @@ public class TimerRoute extends RouteBuilder {  // RouteConfigurationBuilder
 
     @Override
     public void configure() throws Exception {
-        from("timer:foo?period={{timer.period}}")
+        from(timer("ticks").period("{{redelivery.interval}}"))
+                .id("qqqq")
                 .autoStartup(false)
-                .id("ibmmq-amq")
-                .transacted()
-               // .routeId("OutRoute")
-                .setBody().constant("Test data 123")
-                .to("ibmmq:queue:{{ibm.mq.send.QueueName}}")
-                .to("log:TimerRoute")
-                ;
-
-       from("ibmmq:queue:{{ibm.mq.send.QueueName}}")
-               .autoStartup(false)
-                .transacted()
-                .to("log:InRoute");
+            .onException(SQLException.class)
+                .maximumRedeliveries(60)  //.maximumRedeliveryDelay(60*1000) //.backOffMultiplier(2)
+                .redeliveryDelay(15 * 1000L)
+                .handled(true).rollback()
+                .logStackTrace(false)
+                .log(LoggingLevel.ERROR, "${header.CamelExceptionCaught}")
+            .end()
+        .log("Test log message")
+        ;
     }
 
     @Named("tg2filter")
